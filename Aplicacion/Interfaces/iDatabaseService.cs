124420d0a29b32163d67726a5dfa23c5d0886b56
Henry Cangalaya Carrasco <genomikel@gmail.com>
2023-04-30 17:35:59 -0500

    Adding folders to the project’s application layer, and completing the database interface with the domain entities

--- Aplicacion/Interfaces/iDatabaseService.cs
+++ Aplicacion/Interfaces/iDatabaseService.cs
@@ -1,11 +1,41 @@
 ﻿using dominio;
-using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
 
 namespace Aplicacion.Interfaces;
 
 public interface iDatabaseService
 {
-    DbSet<Departamento> Departamentos { get; set; }
+    // Facturas
+    IDbSet<Factura> Facturas { get; set; }
+
+    IDbSet<CambioEstado> CambioEstados { get; set; }
+
+    //Propiedades
+    IDbSet<Departamento> Departamentos { get; set; }
+
+    IDbSet<Cochera> Cocheras { get; set; }
+
+    IDbSet<Almacen> Almacenes { get; set; }
+
+    //Servicios
+    IDbSet<Servicio> Servicios { get; set; }
+
+    IDbSet<CostoVariable> CostoVariables { get; set; }
+
+    IDbSet<Limpieza> Limpiezas { get; set; }
+
+    IDbSet<Mantenimiento> Mantenimientos { get; set; }
+
+    IDbSet<Vigilancia> Vigilancias { get; set; }
+
+    //Vecinos
+    IDbSet<Inquilino> Inquilinos { get; set; }
+
+    IDbSet<Propietario> Propietarios { get; set; }
+
+    IDbSet<Tienda> Tiendas { get; set; }
+
+    IDbSet<Visitante> Visitantes { get; set; }
 
     void Guardar();
 }
